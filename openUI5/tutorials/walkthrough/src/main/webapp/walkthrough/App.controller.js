5a72664d503817f713d5cab643f2180c0fd21162
Wang, Feng <feng01.wang@sap.com>
2016-01-28 17:11:53 +0800

    Step8

--- openUI5/tutorials/walkthrough/src/main/webapp/walkthrough/App.controller.js
+++ openUI5/tutorials/walkthrough/src/main/webapp/walkthrough/App.controller.js
@@ -1,22 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"
-], function(Controller, MessageToast, JSONModel) {
+    "sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel", "sap/ui/model/resource/ResourceModel"
+], function(Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     var privateSayHello = function() {
-        MessageToast.show("Hello World");
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [
+            sRecipient
+        ]);
+        MessageToast.show(sMsg);
     };
-    var privateInit = function() {
+    var setJsonModel = function(thisController) {
         var oData = {
             "recipient": {
                 "name": "World"
             }
         };
         var oModel = new JSONModel(oData);
-        this.getView().setModel(oModel);
+        thisController.getView().setModel(oModel);
+    };
+    var setI18nModel = function(thisController) {
+        var i18nModel = new ResourceModel({
+            bundleName: "openui5.tutorial.i18n.i18n"
+        });
+        thisController.getView().setModel(i18nModel, "i18n");
+    };
+    var init = function() {
+        // since init is called against this controller object, so "this" point to this controller object.
+        // but for plain function call setJsonModel and setI18nModel, "this" will not point to this controller.
+        var thisController = this;
+        setJsonModel(thisController);
+        setI18nModel(thisController);
     };
     var theController = Controller.extend("openui5.tutorial.walkthrough.App", {
         onShowHello: privateSayHello,
-        onInit: privateInit
+        onInit: init
     });
     return theController;
 });
