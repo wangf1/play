d53dd8d7d8b39829e4252deb2d6331cd4a6880d3
Wang, Feng <feng01.wang@sap.com>
2016-01-28 16:52:23 +0800

    Step7

--- openUI5/tutorials/walkthrough/src/main/webapp/walkthrough/App.controller.js
+++ openUI5/tutorials/walkthrough/src/main/webapp/walkthrough/App.controller.js
@@ -1,12 +1,22 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/m/MessageToast"
-], function(Controller, MessageToast) {
+    "sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, JSONModel) {
     "use strict";
     var privateSayHello = function() {
         MessageToast.show("Hello World");
     };
+    var privateInit = function() {
+        var oData = {
+            "recipient": {
+                "name": "World"
+            }
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+    };
     var theController = Controller.extend("openui5.tutorial.walkthrough.App", {
-        onShowHello: privateSayHello
+        onShowHello: privateSayHello,
+        onInit: privateInit
     });
     return theController;
 });
