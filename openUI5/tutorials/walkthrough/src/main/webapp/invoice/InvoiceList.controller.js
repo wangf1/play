c3f53b04d5be2a587748c5e389dc4e33aa86a63a
Wang, Feng <feng01.wang@sap.com>
2016-02-01 15:04:10 +0800

    Step 24: Filtering

--- openUI5/tutorials/walkthrough/src/main/webapp/invoice/InvoiceList.controller.js
+++ openUI5/tutorials/walkthrough/src/main/webapp/invoice/InvoiceList.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "openui5/tutorial/wt/model/formatter"
-], function(Controller, JSONModel, formatter) {
+    "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "openui5/tutorial/wt/model/formatter", "sap/ui/model/Filter", "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
     var init = function() {
         var oViewModel = new JSONModel({
@@ -8,9 +8,20 @@ sap.ui.define([
         });
         this.getView().setModel(oViewModel, "view");
     };
+    var onFilterInvoices = function(oEvent) {
+        var aFilter = [];
+        var sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+            aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+        }
+        var oList = this.getView().byId("invoiceList");
+        var oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
+    };
     var controller = Controller.extend("openui5.tutorial.wt.invoice.InvoiceList", {
         formatter: formatter,
-        onInit: init
+        onInit: init,
+        onFilterInvoices: onFilterInvoices
     });
     return controller;
 });
