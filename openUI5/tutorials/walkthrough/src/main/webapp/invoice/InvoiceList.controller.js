645b86bd8f74ed8d2c9ea82c76006f7be0ffba2a
Wang, Feng <feng01.wang@sap.com>
2016-02-01 13:33:15 +0800

    Step 23: Custom Formatters

--- openUI5/tutorials/walkthrough/src/main/webapp/invoice/InvoiceList.controller.js
+++ openUI5/tutorials/walkthrough/src/main/webapp/invoice/InvoiceList.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+    "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "openui5/tutorial/wt/model/formatter"
+], function(Controller, JSONModel, formatter) {
     "use strict";
     var init = function() {
         var oViewModel = new JSONModel({
@@ -9,6 +9,7 @@ sap.ui.define([
         this.getView().setModel(oViewModel, "view");
     };
     var controller = Controller.extend("openui5.tutorial.wt.invoice.InvoiceList", {
+        formatter: formatter,
         onInit: init
     });
     return controller;
