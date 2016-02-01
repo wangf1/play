3982ab153e777dbc445a359d84e428889a416cec
Wang, Feng <feng01.wang@sap.com>
2016-02-01 13:06:08 +0800

    Step 21: Data Types

--- openUI5/tutorials/walkthrough/src/main/webapp/invoice/InvoiceList.controller.js
+++ openUI5/tutorials/walkthrough/src/main/webapp/invoice/InvoiceList.controller.js
@@ -1,38 +1,15 @@
-sap.ui.controller("openui5.tutorial.wt.invoice.InvoiceList", {
-
-/**
-* Called when a controller is instantiated and its View controls (if available) are already created.
-* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf invoice.InvoiceList
-*/
-//	onInit: function() {
-//
-//	},
-
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf invoice.InvoiceList
-*/
-//	onBeforeRendering: function() {
-//
-//	},
-
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf invoice.InvoiceList
-*/
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf invoice.InvoiceList
-*/
-//	onExit: function() {
-//
-//	}
-
+sap.ui.define([
+    "sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+    "use strict";
+    var init = function() {
+        var oViewModel = new JSONModel({
+            currency: "EUR"
+        });
+        this.getView().setModel(oViewModel, "view");
+    };
+    var controller = Controller.extend("openui5.tutorial.wt.invoice.InvoiceList", {
+        onInit: init
+    });
+    return controller;
 });
