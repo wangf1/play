3ddaa25956be6a397a38b615060a57e39d8483e7
Feng Wang <feng01.wang@sap.com>
2016-02-02 14:36:19 +0800

    Step 33: Routing Back and History

--- openUI5/tutorials/walkthrough/src/main/webapp/main/Detail.controller.js
+++ openUI5/tutorials/walkthrough/src/main/webapp/main/Detail.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"
+], function(Controller, History) {
     "use strict";
     // I return a closure in order to get a reference of the controller
     var createObjectMatchedEventHandler = function(controller) {
@@ -17,8 +17,19 @@ sap.ui.define([
         var onObjectMatched = createObjectMatchedEventHandler(this);
         oRouter.getRoute("detail").attachPatternMatched(onObjectMatched, this);
     };
+    var onNavBack = function() {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
 
+        if (sPreviousHash !== undefined) {
+            window.history.go(-1);
+        } else {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.navTo("overview", true);
+        }
+    };
     return Controller.extend("openui5.tutorial.wt.main.Detail", {
-        onInit: init
+        onInit: init,
+        onNavBack: onNavBack
     });
 });
